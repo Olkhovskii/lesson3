9949806e4c4000b16808543bcfe5cf80b144dff9
Andrei Olkhovskii <andrejolhovskij@MacBook-Pro-Andrej.local>
2024-08-15 00:29:12 +0300

    test is completed 3

--- src/test/java/NewTest.java
+++ src/test/java/NewTest.java
@@ -10,15 +10,13 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class NewTest {
-
     @BeforeAll
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
         Configuration.browser = "chrome";
         Configuration.baseUrl = "https://demoqa.com/automation-practice-form";
         Configuration.pageLoadStrategy = "eager";
-        executeJavaScript("$('#fixedban').remove()");
-        executeJavaScript("$('footer').remove()"); 
+
     }
 
     private static void executeJavaScript(String s) {
@@ -28,6 +26,8 @@ public class NewTest {
     void fillFormTest() {
 
         open("https://demoqa.com/automation-practice-form");
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
         $("#firstName").setValue("Andrew");
         $("#lastName").setValue("Olkhovskii");
         $("#userEmail").setValue("test@email.com");
@@ -41,21 +41,23 @@ public class NewTest {
         $("#hobbiesWrapper").$(byText("Sports")).click();
         $("#uploadPicture").uploadFromClasspath("1.png");
         $("#currentAddress").setValue("Russia");
-        $("div").scrollTo();
+        $("#submit").scrollTo();
         $("#state").click();
         $("#stateCity-wrapper").$(byText("NCR")).click();
         $("#city").click();
         $("#stateCity-wrapper").$(byText("Noida")).click();
         $("#submit").click();
-        $("tbody > tr:nth-child(1) > td:nth-child(2)").shouldHave(text("Andrew Olkhovskii"));
-        $("tr:nth-child(2) > td:nth-child(2)").shouldHave(text("test@email.com"));
-        $("tbody > tr:nth-child(3) > td:nth-child(2)").shouldHave(text("Male"));
-        $("tbody > tr:nth-child(4) > td:nth-child(2)").shouldHave(text("8999777654"));
-        $("tbody > tr:nth-child(5) > td:nth-child(2)").shouldHave(text("17 August,1990"));
-        $("tbody > tr:nth-child(7) > td:nth-child(2)").shouldHave(text("Sports"));
-        $("tbody > tr:nth-child(8) > td:nth-child(2)").shouldHave(text("1.png"));
-        $("tbody > tr:nth-child(9) > td:nth-child(2)").shouldHave(text("Russia"));
-        $("tbody > tr:nth-child(10) > td:nth-child(2)").shouldHave(text("NCR Noida"));
+        $(".table-responsive").$(byText("Student Name")).parent().shouldHave(text("Andrew Olkhovskii"));
+        $(".table-responsive").$(byText("Student Email")).parent().shouldHave(text("test@email.com"));
+        $(".table-responsive").$(byText("Gender")).parent().shouldHave(text("Male"));
+        $(".table-responsive").$(byText("Mobile")).parent().shouldHave(text("8999777654"));
+        $(".table-responsive").$(byText("Date of Birth")).parent().shouldHave(text("17 August,1990"));
+        $(".table-responsive").$(byText("Hobbies")).parent().shouldHave(text("Sports"));
+        $(".table-responsive").$(byText("Picture")).parent().shouldHave(text("1.png"));
+        $(".table-responsive").$(byText("Address")).parent().shouldHave(text("Russia"));
+        $(".table-responsive").$(byText("State and City")).parent().shouldHave(text("NCR Noida"));
         $("#closeLargeModal").click();
     }
 }
+
+
