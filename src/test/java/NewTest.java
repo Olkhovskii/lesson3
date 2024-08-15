a2bbd4287b351110d2eadafa1fca325da5a52c91
Andrei Olkhovskii <andrejolhovskij@MacBook-Pro-Andrej.local>
2024-08-16 00:15:46 +0300

    test is completed 4

--- src/test/java/NewTest.java
+++ src/test/java/NewTest.java
@@ -8,24 +8,24 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class NewTest {
     @BeforeAll
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
         Configuration.browser = "chrome";
-        Configuration.baseUrl = "https://demoqa.com/automation-practice-form";
+        Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
 
     }
 
-    private static void executeJavaScript(String s) {
-    }
+
 
     @Test
     void fillFormTest() {
 
-        open("https://demoqa.com/automation-practice-form");
+        open("/automation-practice-form");
         executeJavaScript("$('#fixedban').remove()");
         executeJavaScript("$('footer').remove()");
         $("#firstName").setValue("Andrew");
@@ -37,7 +37,7 @@ public class NewTest {
         $(".react-datepicker__year-select").selectOption("1990");
         $(".react-datepicker__month-select").selectOption("August");
         $(".react-datepicker__day--017:not(.react-datepicker__day--outside-month)").click();
-        $("#subjectsInput").setValue("Architecture");
+        $("#subjectsInput").setValue("Commerce").pressEnter();
         $("#hobbiesWrapper").$(byText("Sports")).click();
         $("#uploadPicture").uploadFromClasspath("1.png");
         $("#currentAddress").setValue("Russia");
@@ -52,6 +52,7 @@ public class NewTest {
         $(".table-responsive").$(byText("Gender")).parent().shouldHave(text("Male"));
         $(".table-responsive").$(byText("Mobile")).parent().shouldHave(text("8999777654"));
         $(".table-responsive").$(byText("Date of Birth")).parent().shouldHave(text("17 August,1990"));
+        $(".table-responsive").$(byText("Subjects")).parent().shouldHave(text("Commerce"));
         $(".table-responsive").$(byText("Hobbies")).parent().shouldHave(text("Sports"));
         $(".table-responsive").$(byText("Picture")).parent().shouldHave(text("1.png"));
         $(".table-responsive").$(byText("Address")).parent().shouldHave(text("Russia"));
